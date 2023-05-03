e5f7b26be06a7c63f210f42d056c79999dd4840d
Henrique Costa <henrysilva14@gmail.com>
2023-05-03 09:02:35 -0300

    refactoring some configs

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-    print("Testing continuous deployment");
-    print("Nothing is easy");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hey</title>
+</head>
+<body>
+    <?php
+        print("PHP is easy!");
+    ?>
+</body>
+</html>
